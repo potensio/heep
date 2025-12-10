59205f32d5e7506d4a7b2ad7755f059f11bb2dcc
hanifski <h@etalas.com>
2025-12-10 08:44:54 +0700

    build fixes 2

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,10 @@
 const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: "./global.css" });
+// NativeWind v4 metro configuration
+const { withNativeWind } = require("nativewind/metro");
+
+module.exports = withNativeWind(config, {
+  input: "./global.css",
+});
