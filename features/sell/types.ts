76fce87bae1beb34fb4ab1cb2550dd7249d34c73
potensio <hanifyaskur@gmail.com>
2026-05-18 23:21:34 +0700

    feat(sell): add TypeScript types for sell wizard

---
+++ features/sell/types.ts
@@ -0,0 +1,45 @@
+// features/sell/types.ts
+
+export interface SellFormData {
+  photos: string[];
+  name: string;
+  price: number;
+  description: string;
+}
+
+export type WizardStep = 1 | 2 | 3;
+
+export interface SellWizardState {
+  currentStep: WizardStep;
+  formData: SellFormData;
+  isSubmitting: boolean;
+  error: string | null;
+}
+
+export interface PhotoUploadStepProps {
+  photos: string[];
+  onPhotosChange: (photos: string[]) => void;
+  onNext: () => void;
+}
+
+export interface ProductInfoStepProps {
+  formData: SellFormData;
+  onFormChange: (data: Partial<SellFormData>) => void;
+  onNext: () => void;
+  onBack: () => void;
+}
+
+export interface ReviewStepProps {
+  formData: SellFormData;
+  isSubmitting: boolean;
+  onEditPhotos: () => void;
+  onEditInfo: () => void;
+  onPublish: () => void;
+  onBack: () => void;
+}
+
+export interface SuccessScreenProps {
+  productId: string;
+  onViewProduct: () => void;
+  onSellAgain: () => void;
+}
