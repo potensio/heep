7cd2b0193d935142a86674ba0cf3ce4112455e58
potensio <hanifyaskur@gmail.com>
2026-05-20 18:45:27 +0700

    feat(auth): create auth feature folder structure

---
+++ features/auth/components/OtpInput.tsx
@@ -0,0 +1,5 @@
+import { View } from 'react-native';
+
+export function OtpInput() {
+  return <View />;
+}
---
+++ features/auth/index.ts
@@ -0,0 +1,4 @@
+export { PhoneScreen } from './screens/PhoneScreen';
+export { OtpScreen } from './screens/OtpScreen';
+export { CompleteProfileScreen } from './screens/CompleteProfileScreen';
+export { SuccessScreen } from './screens/SuccessScreen';
